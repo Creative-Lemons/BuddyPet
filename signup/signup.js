08dcd25a1b733d2d4a07f0f8626d5bc92040e5ef
malik-shaik <malik43213@gmail.com>
2019-11-13 22:43:48 +0100

    signup.js simplified

--- signup/signup.js
+++ signup/signup.js
@@ -6,16 +6,11 @@ const phone = document.getElementById("phoneInput");
 
 // Form validation
 const validation = () => {
-  const nameValidation = nameValidate(name);
-  const emailValidation = emailValidate(email);
-  const passwordValidation = passwordValidate(pass);
-  const cPasswordValidation = confirmPasswordValidate(confirmPass);
-  const phoneValidation = phoneValidate(phone);
-  return nameValidation &&
-    emailValidation &&
-    passwordValidation &&
-    cPasswordValidation &&
-    phoneValidation
+  return nameValidate(name) &&
+    emailValidate(email) &&
+    passwordValidate(pass) &&
+    confirmPasswordValidate(confirmPass) &&
+    phoneValidate(phone)
     ? true
     : false;
 };
@@ -25,15 +20,9 @@ const nameValidate = el => {
   const sFirstName = el.value;
   const regex = /^[a-zA-Z\s]+$/;
   const nameErr = document.getElementById("nameErr");
-  if (sFirstName.length < 2 || regex.test(sFirstName) === false) {
-    el.style.borderColor = "#e76362";
-    nameErr.style.display = "block";
-    return false;
-  } else {
-    el.style.borderColor = "";
-    nameErr.style.display = "none";
-    return true;
-  }
+  sFirstName.length < 2 || regex.test(sFirstName) === false
+    ? err(el, nameErr)
+    : noErr(el, nameErr);
 };
 
 // Email validation
@@ -41,15 +30,9 @@ const emailValidate = el => {
   const sEmail = el.value;
   const regex = /^\S+@\S+\.\S+$/;
   const emailErr = document.getElementById("emailErr");
-  if (sEmail.length < 6 || regex.test(sEmail) === false) {
-    el.style.borderColor = "#e76362";
-    emailErr.style.display = "block";
-    return false;
-  } else {
-    el.style.borderColor = "";
-    emailErr.style.display = "none";
-    return true;
-  }
+  sEmail.length < 6 || regex.test(sEmail) === false
+    ? err(el, emailErr)
+    : noErr(el, emailErr);
 };
 
 // Password validation
@@ -57,32 +40,19 @@ const passwordValidate = el => {
   const sPassword = el.value;
   const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{5,}$/;
   const passwordErr = document.getElementById("passwordErr");
-  if (sPassword.length < 4 || regex.test(sPassword) === false) {
-    el.style.borderColor = "#e76362";
-    passwordErr.style.display = "block";
-    return false;
-  } else {
-    el.style.borderColor = "";
-    passwordErr.style.display = "none";
-    return true;
-  }
+  sPassword.length < 4 || regex.test(sPassword) === false
+    ? err(el, passwordErr)
+    : noErr(el, passwordErr);
 };
 
 // Confirm password match
 const confirmPasswordValidate = el => {
   const sConfirmPassword = el.value;
   const sPassword = pass.value;
-  // const regex = /^(?=.*\d)(?=.*[a-zA-Z]).{5,}$/;
   const passwordErr = document.getElementById("cPassErr");
-  if (sConfirmPassword.length < 4 || sConfirmPassword !== sPassword) {
-    el.style.borderColor = "#e76362";
-    passwordErr.style.display = "block";
-    return false;
-  } else {
-    el.style.borderColor = "";
-    passwordErr.style.display = "none";
-    return true;
-  }
+  sConfirmPassword.length < 4 || sConfirmPassword !== sPassword
+    ? err(el, passwordErr)
+    : noErr(el, passwordErr);
 };
 
 // Phone number validation
@@ -90,15 +60,9 @@ const phoneValidate = el => {
   const iPhone = el.value;
   const regex = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
   const phoneErr = document.getElementById("phoneErr");
-  if (iPhone.length < 9 || regex.test(iPhone) === false) {
-    el.style.borderColor = "#e76362";
-    phoneErr.style.display = "block";
-    return false;
-  } else {
-    el.style.borderColor = "";
-    phoneErr.style.display = "none";
-    return true;
-  }
+  iPhone.length < 9 || regex.test(iPhone) === false
+    ? err(el, phoneErr)
+    : noErr(el, phoneErr);
 };
 
 // Handle change of member type (user or shelter)
@@ -106,4 +70,16 @@ const handleChange = () => {
   var el = document.getElementById("shelterDivs");
   var memberType = document.getElementById("memberTypeInput").value;
   el.style.display = memberType === "shelter" ? "block" : "none";
+};
+
+// Error functions
+const err = (el, errDiv) => {
+  el.style.borderColor = "#e76362";
+  errDiv.style.display = "block";
+  return false;
+};
+const noErr = (el, errDiv) => {
+  el.style.borderColor = "";
+  errDiv.style.display = "none";
+  return true;
 };
