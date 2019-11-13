23ab959a6c9098f8a1a40c61aee22e3027a50162
malik-shaik <malik43213@gmail.com>
2019-11-13 20:20:49 +0100

    signup with partial styling and validation

--- signup/signup.php
+++ signup/signup.php
@@ -12,55 +12,55 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="test.css">
+    <link rel="stylesheet" href="signup.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="main container">
+    <div class="signup-div container">
         <form method="POST" onsubmit="return validation()">
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="fnameInput">First Name</label>
-                    <input type="text" class="form-control form-control-sm" name="name" id="fnameInput" placeholder="First Name" onkeyup="nameValidate(this)">
+                    <label for="fnameInput">First Name <span class="required">*</span></label>
+                    <input type="text" class="form-control form-control-sm" name="name" id="fnameInput" onkeyup="nameValidate(this)">
                     <div class="invalid-feedback" id="nameErr">First name must be min 2 charecters.</div>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="lnameInput">Last Name</label>
-                    <input type="text" class="form-control form-control-sm" name="name" id="lnameInput" placeholder="Enter Name">
+                    <input type="text" class="form-control form-control-sm" name="name" id="lnameInput">
                     <div class=" invalid-feedback" id="lnameErr">Last name must be min 2 charecters.</div>
 
                 </div>
             </div>
             <div class="form-group">
-                <label for="emailInput">Email address</label>
-                <input type="email" class="form-control form-control-sm" name="email" id="emailInput" placeholder="Enter email" onkeyup="emailValidate(this)">
+                <label for="emailInput">Email address <span class="required">*</span></label>
+                <input type="email" class="form-control form-control-sm" name="email" id="emailInput" onkeyup="emailValidate(this)">
                 <div class="invalid-feedback" id="emailErr">Enter valid email address.</div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="passowrdInput">Password</label>
-                    <input type="password" class="form-control form-control-sm" name="password" id="passwordInput" placeholder="Password" onkeyup="passwordValidate(this)">
+                    <label for="passowrdInput">Password <span class="required">*</span> </label>
+                    <input type="password" class="form-control form-control-sm" name="password" id="passwordInput" onkeyup="passwordValidate(this)">
                     <div class="invalid-feedback" id="passwordErr">Password must be min 4 alpha numeric charecters.</div>
                 </div>
                 <div class="form-group col-md-6">
-                    <label for="cPassInput">Confirm Password</label>
-                    <input type="password" class="form-control form-control-sm" name="cPass" id="cPassInput" placeholder="Retype Password" onkeyup="confirmPasswordValidate(this)">
+                    <label for="cPassInput">Confirm Password <span class="required">*</span></label>
+                    <input type="password" class="form-control form-control-sm" name="cPass" id="cPassInput" onkeyup="confirmPasswordValidate(this)">
                     <div class=" invalid-feedback" id="cPassErr">Passwords do not match.</div>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="phoneInput">Phone</label>
-                <input type="text" class="form-control form-control-sm" name="phone" id="phoneInput" placeholder="Phone" onkeyup="phoneValidate(this)" value="+45 ">
+                <label for="phoneInput">Phone <span class="required">*</span></label>
+                <input type="text" class="form-control form-control-sm" name="phone" id="phoneInput" onkeyup="phoneValidate(this)" value="+45 ">
                 <div class="invalid-feedback" id="phoneErr">Enter valid Phone number.</div>
 
             </div>
 
             <div class="form-group">
-                <label>Address</label>
+                <label>Address <span class="required">*</span></label>
                 <div class="form-row">
                     <div class="form-group col-md-9">
                         <input type="text" class="form-control form-control-sm" name="street" id="streetInput" placeholder="Street name">
@@ -68,18 +68,21 @@ if (isset($_POST['submit'])) {
                     <div class="form-group col-md-3">
                         <input type="text" class="form-control form-control-sm" name="streetno" id="streetnolInput" placeholder="Street no">
                     </div>
-                    <div class="form-group col-md-5">
+                    <div class="form-group col-md-3">
                         <input type="text" class="form-control form-control-sm" name="postalcode" id="postalcodeInput" placeholder="Postalcode">
                     </div>
-                    <div class="form-group col-md-7">
+                    <div class="form-group col-md-6">
                         <input type="text" class="form-control form-control-sm" name="city" id="cityInput" placeholder="City">
+                    </div>
+                    <div class="form-group col-md-3">
+                        <input class="form-control form-control-sm" type="text" placeholder="Denmark" readonly>
                     </div>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="memberTypeInput">You are :</label>
-                <select class="form-control form-control-sm" name="memberType" id="memberTypeInput" onchange="handleChange()">
+                <label for="memberTypeInput">You are : <span class="required">*</span></label>
+                <select class="form-control form-control-sm select-field" name="memberType" id="memberTypeInput" onchange="handleChange()">
                     <option value="user">User</option>
                     <option value="shelter">Shelter</option>
                 </select>
@@ -87,11 +90,11 @@ if (isset($_POST['submit'])) {
 
             <div id="shelterDivs" class="shelterDivs">
                 <div class="form-group">
-                    <label for="shelterName">Shelter Name</label>
+                    <label for="shelterName">Shelter Name <span class="required">*</span></label>
                     <input type="text" class="form-control form-control-sm" name="shelterName" id="shelterName" placeholder="Shelter name">
                 </div>
                 <div class="form-group">
-                    <label for="description">Description</label>
+                    <label for="description">Description <span class="required">*</span></label>
                     <textarea class="form-control" id="Textarea" name="description" placeholder="Enter desciption"></textarea>
                 </div>
                 <div class="form-group">
@@ -106,7 +109,7 @@ if (isset($_POST['submit'])) {
 
 
     <!-- <script src="t.js"></script> -->
-    <script src="test.js"></script>
+    <script src="signup.js"></script>
 </body>
 
 </html>
