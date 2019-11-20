0afcdf7260a9650dc17212c6a55ec59ac7bee6df
BrunaTomaic <57668146+BrunaTomaic@users.noreply.github.com>
2019-11-20 14:18:06 +0100

    Add files via upload

---
+++ pet.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="app1.css">
+    <link href="https://fonts.googleapis.com/css?family=Questrial|Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <title>BuddyPet</title>
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg shadow sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img class="img-responsive logo" src="image/logo3.png"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active mr-4">
+                        <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item mr-4">
+                        <a class="nav-link" href="#">Quiz</a>
+                    </li>
+                    <li class="nav-item mr-4">
+                        <a class="nav-link" href="#">Pet</a>
+                    </li>
+                    <li class="nav-item mr-4">
+                        <a class="nav-link" href="#">Shelters</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sign in</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Full Page Image Header with Vertically Centered Content -->
+    <main>
+
+        <section class="text-center pt-5 pb-5">
+            <p class="title-text-pet1">- ADOPT A PET -</p>
+            <p class="font-weight-bold title-text-pet">Pets</p>
+            <i class="fas fa-paw paw-title"></i>
+        </section>
+
+        <div class="container">
+
+            <!-- For demo purpose 
+    <div class="row py-5">
+      <div class="col-lg-12 mx-auto">
+        <div class="text-white p-5 shadow-sm rounded banner">
+          <h1 class="display-4">Bootstrap 4 photo gallery</h1>
+          <p class="lead">Bootstrap photogallery snippet.</p>
+          <p class="lead">Snippet by <a href="https://bootstrapious.com/snippets" class="text-reset"> 
+                        Bootstrapious</a>, Images by <a href="https://unsplash.com" class="text-reset">Unsplash</a>.
+          </p>
+        </div>
+      </div>
+    </div>-->
+            <!-- End -->
+
+            <div class="row">
+                <!-- Gallery item -->
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+                <!-- End -->
+
+                <!-- Gallery item -->
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="bg-white rounded shadow-sm">
+                        <!--<img src="image/duck1.jpg" alt="" class="img-fluid card-img-top">-->
+                        <div class="hover-mask">
+                            <a href="#" title="">
+                                <img class="img-fluid card-img-top" src="image/duck1.jpg" alt="" />
+                                <h2><span class="glyphicon glyphicon-search">Age: 2 year old <br>Breed: Golden Retiever <br>Gender: Male <br>Personality: Active <br>Shelter: Dyreværnet</span></h2>
+                            </a>
+                        </div>
+                        <div class="p-4  text-center">
+                            <h5> <a href="#" class="text-dark">Duke</a></h5>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+        <!-- End -->
+
+        <!-- Gallery item 
+      <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
+        <div class="bg-white rounded shadow-sm"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556294928/nicole-honeywill-546848-unsplash_ymprvp.jpg" alt="" class="img-fluid card-img-top">
+          <div class="p-4">
+            <h5> <a href="#" class="text-dark">And She Realized</a></h5>
+            <p class="small text-muted mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+            <div class="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
+              <p class="small mb-0"><i class="fa fa-picture-o mr-2"></i><span class="font-weight-bold">JPG</span></p>
+              <div class="badge badge-warning px-3 rounded-pill font-weight-normal text-white">Featured</div>
+            </div>
+          </div>
+        </div>
+      </div>-->
+        <!-- End -->
+
+
+
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <h1 class="fotter-title-name mt-3 mb-3">We are Creative Lemons</h1>
+                        <p class="fotter-text">We help people and pets find each other and live happier lives together. That's our mission. It's why we exist.</p>
+                    </div>
+                    <div class="col-sm ml-5">
+                        <h1 class="fotter-title mt-3 mb-3"><i class="fas fa-address-book mr-2"></i>Contact us</h1>
+                        <p class="fotter-text mb-2"><i class="fas fa-phone mr-2"></i>(45) 45 36 89 54</p>
+                        <p class="fotter-text mb-2"><i class="fas fa-envelope mr-2"></i>email@yoursite.com</p>
+                        <p class="fotter-text"><i class="fas fa-map-marker-alt mr-2"></i>Pet Street 123 – Copenhagen</p>
+                    </div>
+                    <div class="col-sm ml-5">
+                        <h1 class="fotter-title mt-3 mb-3"><i class="fas fa-hashtag mr-2"></i>Social Media</h1>
+                        <p class="fotter-text mb-2"><i class="fab fa-facebook-square mr-2"></i>Facebook</p>
+                        <p class="fotter-text mb-2"><i class="fab fa-instagram mr-2"></i>Instagram</p>
+                        <p class="fotter-text"><i class="fab fa-linkedin mr-2"></i>Linkedin</p>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright m-0 text-center pb-2">&copy; Copyright 2019 Creative Lemons</p>
+        </footer>
+
+    </main>
+
+</body>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</html>
+
+<?php
+?>
