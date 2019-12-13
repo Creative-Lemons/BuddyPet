6f277231ad23775c3de2a8d9b732340a9a30e585
Elaheh <elah0048@stud.kea.dk>
2019-12-13 11:24:04 +0100

    Add files via upload

--- adopt-me.js
+++ adopt-me.js
@@ -15,13 +15,36 @@ function closeModal() {
   document.querySelector("body").classList.remove("overflow");
   document.querySelector("#back-drop-adopt").classList.add("hidden");
 }
-document.querySelector("#close-adopt-btn").addEventListener("click", closeModal);
+document
+  .querySelector("#close-adopt-btn")
+  .addEventListener("click", closeModal);
 function closeModal() {
-  console.log("hi");
   document.querySelector("#pop-up-adopt").classList.remove("fade-in-animation");
-  //   document.querySelector("#pop-up").classList.add("hidden");
-
   document.querySelector("#pop-up-adopt").classList.add("fade-out-animation");
+  document.querySelector("body").classList.remove("overflow");
+  document.querySelector("#back-drop-adopt").classList.add("hidden");
+}
+document
+  .querySelector(".btn-schedule-check")
+  .addEventListener("click", setVisitFeedback);
+function setVisitFeedback() {
+  document.querySelector("#pop-up-set-vist").classList.remove("hidden");
+  document.querySelector("#pop-up-set-vist").classList.add("fade-in-animation");
+  document.querySelector("#back-drop-adopt").classList.remove("hidden");
+  document.querySelector("body").classList.add("overflow");
+}
+document
+  .querySelector("#close-visit-btn")
+  .addEventListener("click", closeVisit);
+document.querySelector("#close-visit").addEventListener("click", closeVisit);
+
+function closeVisit() {
+  document
+    .querySelector("#pop-up-set-vist")
+    .classList.remove("fade-in-animation");
+  document
+    .querySelector("#pop-up-set-vist")
+    .classList.add("fade-out-animation");
   document.querySelector("body").classList.remove("overflow");
   document.querySelector("#back-drop-adopt").classList.add("hidden");
 }
